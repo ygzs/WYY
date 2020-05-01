731021dbf9d0068ff2405007b69b25e965a17085
ygzs <1103449238@qq.com>
2020-05-01 19:09:42 +0800

    连接到数据库

--- src/js/main.js
+++ src/js/main.js
@@ -15,13 +15,6 @@
         `,
         render(data){
             let placeholder = ['songName','singer','link']
-            let {name,link} = data
-            let singerSongType = name.split('.')
-            let singerSong = singerSongType[0]
-            let singerAndSong = singerSong.split('-')
-            data = {songName:singerAndSong[1],
-                    singer:singerAndSong[0],
-                    link:link}
             let html = this.template
             placeholder.map((item)=>{
                 html = html.replace(`**${item}**`,data[item])
@@ -29,7 +22,9 @@
             $(this.el).append(html)
         },
     }
-    let model = {}
+    let model = {
+        data:{}
+    }
     let controller = {
         init(view,model){
             this.view = view
@@ -38,8 +33,28 @@
         },
         bindEvents:function(){
             window.eventHub.on('upload',(data)=>{
+                let {name,link} = data
+                let singerSongType = name.split('.')
+                let singerSong = singerSongType[0]
+                let singerAndSong = singerSong.split('-')
+                data = {songName:singerAndSong[1],
+                        singer:singerAndSong[0],
+                        link:link}
+                this.model.data = data 
                 this.view.render(data)
+                let string = JSON.stringify(this.model.data)
+                let object = JSON.parse(string)
+                this.savemessage(object)
             })
+        },
+        savemessage:function(object){
+            let {songName,singer,link} = object
+            const Song = AV.Object.extend('Songs')
+            const song = new Song()
+            song.set('songName', songName)
+            song.set('singer', singer)
+            song.set('link', link)
+            song.save()
         },
     }
     controller.init(view,model)
